0ce454edd940cecefe30068e92bb86278a2d554a
PEDRO <cg3011411@LAB201M08.IFSP>
2022-08-25 21:31:18 -0300

    Adding image upload to the products

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -16,5 +16,6 @@ class Produto extends Model
         'categoria_id',
         'preco',
         'quantidade',
+        'imagem'
     ];
 }
--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -13,50 +13,60 @@
     <h1 class="text-center my-4">CADASTRAR PRODUTO</h1>
     <div class="card shadow my-5 w-75 mx-auto border border-3" style="border-radius: 20px;">
         <div class="card-body">
-            <form action="{{ route('produtos.createP.store') }}" method="POST">
+            <form action="{{ route('produtos.createP.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
                             <label for="nome">Nome</label>
-                            <input type="text" name="nome" id="nome" class="form-control {{ $errors->has('nome') ? 'is-invalid' : ''}}" placeholder="Digite o nome do produto">
-                            <div class="invalid-feedback">{{ $errors->first('nome')}}</div>
+                            <input type="text" name="nome" id="nome"
+                                class="form-control {{ $errors->has('nome') ? 'is-invalid' : '' }}"
+                                placeholder="Digite o nome do produto">
+                            <div class="invalid-feedback">{{ $errors->first('nome') }}</div>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="categoria_id">Categoria</label>
-                            <select name="categoria_id" id="categoria_id" class="form-control {{ $errors->has('categoria_id') ? 'is-invalid' : ''}}">
+                            <select name="categoria_id" id="categoria_id"
+                                class="form-control {{ $errors->has('categoria_id') ? 'is-invalid' : '' }}">
                                 <option value="">Escolha uma categoria...</option>
-                                <option value="teste">teste</option>
+                                <option value="1">teste</option>
                                 <!-- Foreach com as categorias -->
                             </select>
-                            <div class="invalid-feedback">{{ $errors->first('categoria_id')}}</div>
+                            <div class="invalid-feedback">{{ $errors->first('categoria_id') }}</div>
                         </div>
                     </div>
-
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="preco">Preço</label>
-                            <input type="text" name="preco" id="preco" class="form-control {{ $errors->has('preco') ? 'is-invalid' : ''}}" placeholder="Digite o valor do produto">
-                            <div class="invalid-feedback">{{ $errors->first('preco')}}</div>
-
+                            <input type="text" name="preco" id="preco"
+                                class="form-control {{ $errors->has('preco') ? 'is-invalid' : '' }}"
+                                placeholder="Digite o valor do produto">
+                            <div class="invalid-feedback">{{ $errors->first('preco') }}</div>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="quantidade">Quantidade</label>
-                            <input type="number" name="quantidade" id="quantidade" class="form-control {{ $errors->has('quantidade') ? 'is-invalid' : ''}}" placeholder="Informe a quantidade do produto">
-                            <div class="invalid-feedback">{{ $errors->first('quantidade')}}</div>
+                            <input type="number" name="quantidade" id="quantidade"
+                                class="form-control {{ $errors->has('quantidade') ? 'is-invalid' : '' }}"
+                                placeholder="Informe a quantidade do produto">
+                            <div class="invalid-feedback">{{ $errors->first('quantidade') }}</div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="imagem">Imagem</label>
+                            <input type="file" name="imagem" id="imagem" class="form-control p-1" required>
                         </div>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-success btn-block mt-3">
                     Enviar
                 </button>
+            </form>
         </div>
-        </form>
-    </div>
     </div>
 </body>
 
